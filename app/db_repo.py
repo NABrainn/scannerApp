480d13ed15794c4a698233529895f8096c47b9be
NAbrain <michal2002.60@gmail.com>
2024-11-30 09:06:48 +0100

    delete

--- app/db_repo.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Result, Row
-from .config import Config
-from sqlalchemy.orm import Session
-
-def query_first(stmt) -> Row:
-    with Session(Config.engine) as session:
-        val = session.execute(stmt).first()
-    return val
-    
