ffc54d604bdb8397ef5ebd3ab5ae65cdf124d606
NAbrain <michal2002.60@gmail.com>
2024-11-30 09:06:42 +0100

    add config.py module to manage config for different environments

--- app/config.py
+++ app/config.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine
-from flask_htmx import HTMX
 
 
 class Config:
-    engine = create_engine("sqlite:///scannerappdb", echo=True)
+    SECRET_KEY = 'b69e5f14067dc6fd89dc321cdd3ed05f29f2854c9e0aa5c199b117dd511e25c0'
+    TESTING = False
 
+class ProductionConfig(Config):
+    DATABASE_URI = "sqlite:///scannerappdb"
 
-def init_db():
-    pass
+class DevelopmentConfig(Config):
+    DATABASE_URI = "sqlite:///scannerappdb"
+    ENGINE = create_engine(DATABASE_URI, echo=True)
+
+class TestingConfig(Config):
+    DATABASE_URI = "sqlite:///scannerappdb"
+
